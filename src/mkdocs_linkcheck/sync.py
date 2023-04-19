bb4476b98ce4a553354e8e1a66d338542e5ce80a
Sebastian Blasius <sebastian.blasius@waterlinked.com>
2023-04-19 11:16:01 +0200

    synchronous external link check
     * changed to check for status code, not just except any page
     * more debug output

--- src/mkdocs_linkcheck/sync.py
+++ src/mkdocs_linkcheck/sync.py
@@ -41,25 +41,30 @@ def check_urls(
                 R = sess.head(url, allow_redirects=True, timeout=TIMEOUT, verify=verifycert)
                 if R.status_code in RETRYCODES:
                     if retry(url, hdr, verifycert):
+                        logging.info(f"OK RETRY: {url:80s}")
                         continue
                     else:
                         #yield u, url, R.status_code
                         missing.append( [ u['fn'], u['url'], R.status_code ] )
+                        logging.info(f"NOT FOUND RETRY: {R.status_code} {url:80s}")
                         continue
             except OKE:
+                logging.info(f"OK MANY REDIRECTS: {url:80s}")
                 continue
             except EXC as e:
                 if retry(url, hdr, verifycert):
+                    logging.info(f"OK RETRY EXC: {url:80s}")
                     continue
                 missing.append( [ u['fn'], u['url'], str(e) ] )
                 #yield u, url, str(e)
+                logging.info(f"NOT FOUND EXC: {e} {url:80s}")
                 continue
 
             code = R.status_code
             if code != 200:
                 #yield u, url, code
                 missing.append( [ u['fn'], u['url'], code ] )
-                logging.info(f'NOT FOUND: {url:80s}')
+                logging.info(f"NOT FOUND: {R.status_code} {url:80s}")
             else:
                 logging.info(f"OK: {url:80s}")
 
@@ -69,13 +74,11 @@ def retry(url: str, hdr: dict[str, str] = None, verifycert: bool = False) -> boo
     ok = False
     try:
         # anti-crawling behavior doesn't like .head() method--.get() is slower but avoids lots of false positives
-        with requests.get(
+        R = requests.get(
             url, allow_redirects=True, timeout=TIMEOUT, verify=verifycert, headers=hdr, stream=True
-        ) as stream:
-            Rb = next(stream.iter_lines(80), None)
-            # if Rb is not None and 'html' in Rb.decode('utf8'):
-            if Rb and len(Rb) > 10:
-                ok = True
+        )
+        if R.status_code == 200:
+            ok = True
     except EXC:
         pass
 
